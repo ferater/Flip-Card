48f7c11d26b6e81ce944bc6f4fa6cb711aaced6c
Ferhat ER <lersmooth@gmail.com>
2023-05-31 18:09:39 +0300

    try to animate on mouseleavr

---
+++ app.js
@@ -0,0 +1,8 @@
+const cardContainer = document.querySelector(".card-container");
+const card = cardContainer.querySelector(".card");
+// cardContainer.addEventListener("mouseleave", () => {
+//   card.classList.add("animate");
+// });
+// card.addEventListener("animationend", () => {
+//   card.classList.remove("animate");
+// });
